462028e1125d76e85ecad608dece783343b42341
kadoyukasi <63542479+kadoyukasi@users.noreply.github.com>
2025-01-18 18:11:08 +0900

    feat: configure aliases and abbrreviations

--- .zsh/alias/gnu.zsh
+++ .zsh/alias/gnu.zsh
@@ -1,4 +1,3 @@
-abbr ls='gls --color=auto --show-control-chars'
-abbr ll='gls -l --color=auto --show-control-chars'
-abbr la='gls -la --color=auto --show-control-chars'
-abbr grep='ggrep --color=auto'
+# GNU compatible aliases
+alias grep='ggrep --color=auto'
+alias ls='gls --color=auto --show-control-chars'
