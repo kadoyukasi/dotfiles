61ab912ba74b53db5927c01d73f87209a39d2c05
kadoyukasi <63542479+kadoyukasi@users.noreply.github.com>
2025-01-26 16:53:11 +0900

    feat: brushup git-cb cmd

--- .zsh/alias/git.zsh
+++ .zsh/alias/git.zsh
@@ -1,12 +1,18 @@
 # expect olets/zsh-abbr
 alias git-cleanup-branches='
   git fetch --prune
-  remote_branches=$(git branch -r | awk "{print \$1}" | sed "s|origin/||")
+  current_branch=$(git rev-parse --abbrev-ref HEAD)
+  remote_branches=$(git branch -r | sed "s|origin/||" | grep -v "HEAD")
   local_branches=$(git branch --format="%(refname:short)")
 
-  for branch in $local_branches; do
-    if ! echo "$remote_branches" | grep -qx "$branch"; then
-      git branch -D "$branch"
+  echo "$local_branches" | while read branch; do
+    if [ "$branch" != "$current_branch" ]; then
+      if ! echo "$remote_branches" | grep -qx "$branch"; then
+        echo "Deleting local branch: $branch"
+        git branch -D "$branch"
+      fi
+    else
+      echo "Skipping current branch: $branch"
     fi
   done
 '
